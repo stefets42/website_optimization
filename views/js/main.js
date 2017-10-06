78a63700afe4cf58654bb5f55f8f63fad6fd39f7
stefets42 <stefets42@users.noreply.github.com>
2017-10-06 19:51:36 +0200

    Updated file

--- views/js/main.js
+++ views/js/main.js
@@ -445,11 +445,12 @@ var resizePizzas = function(size) {
     return dx;
   }
 
+    var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
+    var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
+
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
     for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
       document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
     }
   }
